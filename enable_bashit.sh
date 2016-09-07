66c0b81f2afa7ff0e9dc46275e161cdde97b9c34
Paulo Eduardo <pauloelr@gmail.com>
2016-09-07 20:25:36 -0300

    Update Bash-It
    The lasts installation of the Castle have caused the bash to not run properly,
    seems like that's being caused by a change on bash-it, so in order to solve
    the problem i tried to recreate a default installation of bash-it to see what
    could have be causing the problem
    
    After some digging a think the problem is related to the .bashrc and
    .bash_profile files being symlink, what can cause a circular loading
    of the files and crash bash terminal
    
    Some files were also with incorrect file endings, that has being also fixed
    by this commit

--- enable_bashit.sh
+++ enable_bashit.sh
@@ -3,8 +3,11 @@
 source ~/.bash_it/bash_it.sh
 
 # Aliases
+bash-it enable alias apt
+bash-it enable alias atom
 bash-it enable alias general
 bash-it enable alias git
+bash-it enable alias npm
 bash-it enable alias vagrant
 bash-it enable alias vim
 
@@ -14,15 +17,17 @@ bash-it enable completion defaults
 bash-it enable completion git
 bash-it enable completion git_flow
 bash-it enable completion homesick
+bash-it enable completion npm
 bash-it enable completion ssh
 
 # Plugins
+bash-it enable plugin alias-completion
 bash-it enable plugin base
 bash-it enable plugin battery
 bash-it enable plugin browser
 bash-it enable plugin dirs
 bash-it enable plugin extract
 bash-it enable plugin git
+bash-it enable plugin history
 bash-it enable plugin javascript
 bash-it enable plugin ssh
-bash-it enable plugin vagrant
--- home/.bash_it/aliases/custom.aliases.bash
+++ home/.bash_it/aliases/custom.aliases.bash
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
